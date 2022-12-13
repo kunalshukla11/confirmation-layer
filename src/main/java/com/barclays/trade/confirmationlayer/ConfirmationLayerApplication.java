0dd5cb9885bf22f964df96121d9e4f9bcabd1272
Nrn Shukla <narendra1192cs@gmail.com>
2022-12-13 18:58:28 +0000

    Initial commit

---
+++ src/main/java/com/barclays/trade/confirmationlayer/ConfirmationLayerApplication.java
@@ -0,0 +1,13 @@
+package com.barclays.trade.confirmationlayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfirmationLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfirmationLayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barclays/trade/confirmationlayer/ConfirmationLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barclays.trade.confirmationlayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfirmationLayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
